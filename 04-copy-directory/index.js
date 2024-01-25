d2ae7c7a54fa2958d56b080ac68e50aa0ab1b55a
Danila <87062165+TJWGMK5G@users.noreply.github.com>
2024-01-25 10:36:35 +0300

    fix: Code change and fulfillment of all conditions

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,18 +1,43 @@
+// const fs = require('fs').promises;
+// const path = require('path');
+
+// async function copyDir() {
+//   const sourceDir = '04-copy-directory/files';
+//   const destinationDir = '04-copy-directory/files-copy';
+
+//   try {
+//     const files = await fs.readdir(sourceDir);
+
+//     for (const file of files) {
+//       const sourceFilePath = path.join(sourceDir, file);
+//       const destinationFilePath = path.join(destinationDir, file);
+
+//       await fs.copyFile(sourceFilePath, destinationFilePath);
+//       console.log(`Файл ${file} успешно скопирован.`);
+//     }
+
+//     console.log('Копирование завершено.');
+//   } catch (err) {
+//     console.error('Произошла ошибка при копировании:', err);
+//   }
+// }
+
+// copyDir();
+
 const fs = require('fs').promises;
 const path = require('path');
 
-async function copyDir() {
-  const sourceDir = '04-copy-directory/files';
-  const destinationDir = '04-copy-directory/files-copy';
-
+async function copyDir(source, destination) {
   try {
-    const files = await fs.readdir(sourceDir);
+    await fs.mkdir(destination, { recursive: true });
+
+    const files = await fs.readdir(source);
 
     for (const file of files) {
-      const sourceFilePath = path.join(sourceDir, file);
-      const destinationFilePath = path.join(destinationDir, file);
+      const [sourcePath, destinationPath] = [path.join(source, file), path.join(destination, file)];
 
-      await fs.copyFile(sourceFilePath, destinationFilePath);
+      await fs.writeFile(destinationPath, await fs.readFile(sourcePath));
+
       console.log(`Файл ${file} успешно скопирован.`);
     }
 
@@ -22,4 +47,7 @@ async function copyDir() {
   }
 }
 
-copyDir();
+const sourceDir = '04-copy-directory/files';
+const destinationDir = '04-copy-directory/files-copy';
+
+copyDir(sourceDir, destinationDir);

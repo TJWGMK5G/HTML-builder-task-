f18a4c3c1f46f1c9fdf8e384cab88bc67334ad48
Danila <87062165+TJWGMK5G@users.noreply.github.com>
2024-01-25 10:17:37 +0300

    fix: Code change and fulfillment of all conditions

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,10 +1,47 @@
-const fs = require('fs').promises;
-const readline = require('readline').createInterface({ input: process.stdin, output: process.stdout });
+// const fs = require('fs').promises;
+// const readline = require('readline').createInterface({ input: process.stdin, output: process.stdout });
 
-(async () => {
-  const userInput = await new Promise(resolve => readline.question('Greetings! Please enter some text: ', resolve));
-  await fs.writeFile('02-write-file/textTest.txt', userInput, 'utf-8');
-  console.log('Text successfully written to "textTest.txt".');
+// (async () => {
+//   const userInput = await new Promise(resolve => readline.question('Greetings! Please enter some text: ', resolve));
+//   await fs.writeFile('02-write-file/textTest.txt', userInput, 'utf-8');
+//   console.log('Text successfully written to "textTest.txt".');
+//   readline.close();
+//   process.exit();
+// })();
+
+
+const fs = require('fs').promises;
+const readline = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+const filePath = '02-write-file/textTest.txt';
+
+const exitProcess = () => {
+  console.log('\nFarewell! Exiting the process.');
   readline.close();
   process.exit();
+};
+
+process.on('SIGINT', exitProcess);
+
+(async () => {
+  try {
+    await fs.access(filePath);
+    console.log('File exists. Appending new text.');
+  } catch (err) {
+    console.log('File does not exist. Creating a new file.');
+  }
+
+  while (true) {
+    const userInput = await new Promise(resolve =>
+      readline.question('Enter text (or type "exit" to quit): ', resolve)
+    );
+
+    if (userInput.toLowerCase() === 'exit') exitProcess();
+
+    await fs.appendFile(filePath, userInput + '\n', 'utf-8');
+    console.log('Text appended to "textTest.txt".');
+  }
 })();
